88c424cfeca75eb453d37b1cfbb94a3c05e76963
isanem <35311680+isanem@users.noreply.github.com>
2021-06-06 18:59:44 +0200

    Fixed problem with footer on contact page

---
+++ assets/js/burger.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+    burger();
+
+    function burger() {
+        let burgerNav = $('.burger-nav');
+        let burgerNavLinks = $('.nav-links');
+
+        burgerNav.on('click', function() {
+            burgerNavLinks.toggleClass('burger-nav-active');
+
+            
+        })
+    }
+});

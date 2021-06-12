adccfde05e060ec6b1f979df01e622921e535d7d
isanem <35311680+isanem@users.noreply.github.com>
2021-06-12 23:44:31 +0200

    Fixed responsivness on contact page

--- assets/js/burger.js
+++ assets/js/burger.js
@@ -1,15 +1,20 @@
 $(document).ready(function(){
     burger();
-
+    
     function burger() {
         let burgerNav = $('.burger-nav');
         let burgerNavLinks = $('.nav-links');
-
+        
         burgerNav.on('click', function() {
             burgerNavLinks.toggleClass('burger-nav-active');
             burgerNav.toggleClass('toggle');
             
         })
 
+        burgerNavLinks.on('click', function() {
+            burgerNavLinks.toggleClass('burger-nav-active');
+            burgerNav.toggleClass('toggle');
+
+        })
     }
 });

0aeff4750c207cf772a13ee6fdf927dcc1419a38
isanem <35311680+isanem@users.noreply.github.com>
2021-06-18 21:56:49 +0200

    Last corrections in place. Website ready for deplyment

--- assets/js/textslide.js
+++ assets/js/textslide.js
@@ -1,27 +1,25 @@
-$(document).ready(function(){
-
+$(document).ready(function () {
     textSlide();
-    
+
     function textSlide() {
         let i = 0;
         let text = $('#p-overlay');
         let textArray = [
-            "Bygglovshandlingar",
-            "Rådgivning",
-            "Bygghandlingar",
-            "Visualisering i 3D",
+            "Bygglovshandlingar?",
+            "Rådgivning?",
+            "Bygghandlingar?",
+            "Visualisering i 3D?",
+            "Idé till skiss?"
         ];
-        
-        text.html(textArray[i]);
 
-        setInterval(function() {
+        text.html(textArray[i]);
+        setInterval(function () {
             i++;
 
             text.html(textArray[i]);
-            if (i >= textArray.length) {
-                i = 0;
+            if (i >= (textArray.length - 1)) {
+                i = -1;
             }
-            
         }, 1500);
     }
 });
